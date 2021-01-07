da5c40090ff409d8bc2d4fd27c84160229690161
Rajiv Seelam <rajiv@betalectic.com>
2021-01-07 23:31:58 +0530

    Try something else

--- app/Http/Controllers/WOPIController.php
+++ app/Http/Controllers/WOPIController.php
@@ -16,25 +16,25 @@ class WOPIController extends Controller
 
     function checkFileInfo($fileId)
     {
-        $filePath = public_path('sample.docx');
+        $filePath = public_path('test.doc');
 
         $handle = fopen($filePath, "r");
         $size = filesize($filePath);
         $contents = fread($handle, $size);
 
         return [
-            "BaseFileName" => "sample.docx",
+            "BaseFileName" => "test.doc",
             "Size" => $size,
             "OwnerId" => 1,
             "UserId" => 1,
             "Version" => rand(),
-            "FileUrl" => url("sample.docx")
+            "FileUrl" => url("test.doc")
         ];
     }
 
     function getFile($fileId)
     {
-        $filePath = public_path('sample.docx');
+        $filePath = public_path('test.doc');
         header("Content-Type: application/octet-stream");
         // $contents = file_get_contents($filePath);
 
@@ -42,7 +42,7 @@ class WOPIController extends Controller
         $contents = fread($handle, filesize($filePath));
 
         return response()
-            ->download($filePath, "sample.docx",[
+            ->download($filePath, "test.doc",[
                 'Content-Type' => 'application/octet-stream'
             ]);
 
