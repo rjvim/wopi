3c8f8aec129a8d217e6a51c2f158fa519371f53f
Rajiv Seelam <rajiv@betalectic.com>
2021-01-07 23:50:48 +0530

    Try something else

--- app/Http/Controllers/WOPIController.php
+++ app/Http/Controllers/WOPIController.php
@@ -42,6 +42,7 @@ class WOPIController extends Controller
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Content-Transfer-Encoding: binary');
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
             header('Pragma: public');
