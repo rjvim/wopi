2ffb184b7abf57e9f42b914325f78674a004a6d5
Rajiv Seelam <rajiv@betalectic.com>
2021-01-15 15:56:14 +0530

    #time 2h Make changes to wopi controller

--- app/Http/Controllers/WOPIController.php
+++ app/Http/Controllers/WOPIController.php
@@ -17,24 +17,55 @@ class WOPIController extends Controller
 
     function checkFileInfo($fileId)
     {
+        $fileName = "sample.docx";
+
         $filePath = public_path('sample.docx');
 
-        $handle = fopen($filePath, "r");
-        $size = filesize($filePath);
-        $contents = fread($handle, $size);
 
-        return [
-            "BaseFileName" => "sample.docx",
-            "Size" => $size,
-            "OwnerId" => 1,
-            "UserId" => 1,
-            "Version" => rand(),
-            "FileUrl" => url("sample.docx")
-        ];
+        $FileInfoDto = array(
+            'BaseFileName' => $fileName,
+            'OwnerId' => 'admin',
+            'ReadOnly' => true,
+            'Size' => $filePath,
+            'Version' => rand()
+        );
+
+        $jsonString = json_encode($FileInfoDto);
+        header('Content-Type: application/json');
+        echo $jsonString;
+
+
+        // $handle = fopen($filePath, "r");
+        // $size = filesize($filePath);
+        // $contents = fread($handle, $size);
+
+        // return [
+        //     "BaseFileName" => "sample.docx",
+        //     "Size" => $size,
+        //     "OwnerId" => 'admin',
+        //     "ReadOnly" => true,
+        //     "UserId" => 1,
+        //     "Version" => rand(),
+        //     "FileUrl" => url("sample.docx")
+        // ];
     }
 
     function getFile($fileId)
     {
+        $fileName = "sample.docx";
+
+        $filePath = public_path('sample.docx');
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($filePath));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filePath));
+        readfile($filePath);
+        exit;
+
         $file = public_path('sample.docx');
 
         return new BinaryFileResponse($file);
